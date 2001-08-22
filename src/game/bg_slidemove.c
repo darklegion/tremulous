afe568e607fd020563fc4e2acba45f9521cf1869
Tim Angus <tim@ngus.net>
2001-08-22 01:40:12 +0000

    Fixed something I missed between 1.27->1.29

--- src/game/bg_slidemove.c
+++ src/game/bg_slidemove.c
@@ -241,7 +241,7 @@ void PM_StepSlideMove( qboolean gravity ) {
 //	float		down_dist, up_dist;
 //	vec3_t		delta, delta2;
 	vec3_t		up, down;
-  float   stepSize;
+	float		stepSize;
 
 	VectorCopy (pm->ps->origin, start_o);
 	VectorCopy (pm->ps->velocity, start_v);
@@ -267,7 +267,7 @@ void PM_StepSlideMove( qboolean gravity ) {
 	up[2] += STEPSIZE;
 
 	// test the player position if they were a stepheight higher
-  pm->trace (&trace, start_o, pm->mins, pm->maxs, up, pm->ps->clientNum, pm->tracemask);
+	pm->trace (&trace, start_o, pm->mins, pm->maxs, up, pm->ps->clientNum, pm->tracemask);
 	if ( trace.allsolid ) {
 		if ( pm->debugLevel ) {
 			Com_Printf("%i:bend can't step\n", c_pmove);
@@ -275,16 +275,16 @@ void PM_StepSlideMove( qboolean gravity ) {
 		return;		// can't step up
 	}
 
-  stepSize = trace.endpos[2] - start_o[2];
+	stepSize = trace.endpos[2] - start_o[2];
 	// try slidemove from this position
-  VectorCopy (trace.endpos, pm->ps->origin);
+	VectorCopy (trace.endpos, pm->ps->origin);
 	VectorCopy (start_v, pm->ps->velocity);
 
 	PM_SlideMove( gravity );
 
 	// push down the final amount
 	VectorCopy (pm->ps->origin, down);
-	down[2] -= STEPSIZE;
+	down[2] -= stepSize;
 	pm->trace (&trace, pm->ps->origin, pm->mins, pm->maxs, down, pm->ps->clientNum, pm->tracemask);
 	if ( !trace.allsolid ) {
 		VectorCopy (trace.endpos, pm->ps->origin);
@@ -326,4 +326,3 @@ void PM_StepSlideMove( qboolean gravity ) {
 		}
 	}
 }
-
