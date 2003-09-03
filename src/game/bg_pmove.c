fd5fb23fed5ac81d2da7653d5692aa5e0cc91c7c
Tim Angus <tim@ngus.net>
2003-09-03 00:25:14 +0000

    * Added UpdateViewAngles back in to PM_NOCLIP

--- src/game/bg_pmove.c
+++ src/game/bg_pmove.c
@@ -3095,6 +3095,7 @@ void PmoveSingle (pmove_t *pmove)
 
   if( pm->ps->pm_type == PM_NOCLIP )
   {
+    PM_UpdateViewAngles( pm->ps, &pm->cmd );
     PM_NoclipMove( );
     PM_DropTimers( );
     return;
