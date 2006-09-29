e6febd833c2e99780d0a18bed33b116bf6d032f2
Tim Angus <tim@ngus.net>
2006-09-29 23:07:28 +0000

    * Changed a Com_Printf to a Com_DPrintf

--- src/client/snd_dma.c
+++ src/client/snd_dma.c
@@ -541,7 +541,7 @@ void S_Base_StartSound(vec3_t origin, int entityNum, int entchannel, sfxHandle_t
 					}
 				}
 				if (chosen == -1) {
-					Com_Printf("dropping sound\n");
+					Com_DPrintf("dropping sound\n");
 					return;
 				}
 			}
