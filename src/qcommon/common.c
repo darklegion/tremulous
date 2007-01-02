bfc7853860c7c40d1c11d34b2c5f92b1a2428229
Tim Angus <tim@ngus.net>
2007-01-02 14:13:04 +0000

    * Update demo_protocols

--- src/qcommon/common.c
+++ src/qcommon/common.c
@@ -33,7 +33,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #endif
 
 int demo_protocols[] =
-{ 66, 67, 68, 69, 0 };
+{ PROTOCOL_VERSION, 0 };
 
 #define MAX_NUM_ARGVS	50
 
