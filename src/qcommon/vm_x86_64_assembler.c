cd08e6a463d61d20c1e622b623c7f00c1df7a502
Tim Angus <tim@ngus.net>
2007-09-06 14:07:23 +0000

    * De-quake

--- src/qcommon/vm_x86_64_assembler.c
+++ src/qcommon/vm_x86_64_assembler.c
@@ -4,18 +4,18 @@ vm_x86_64_assembler.c -- assembler for x86-64
 
 Copyright (C) 2007 Ludwig Nussel <ludwig.nussel@suse.de>, Novell inc.
 
-Quake III Arena source code is free software; you can redistribute it
+Tremulous is free software; you can redistribute it
 and/or modify it under the terms of the GNU General Public License as
 published by the Free Software Foundation; either version 2 of the License,
 or (at your option) any later version.
 
-Quake III Arena source code is distributed in the hope that it will be
+Tremulous is distributed in the hope that it will be
 useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with Quake III Arena source code; if not, write to the Free Software
+along with Tremulous; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 ===========================================================================
 */
