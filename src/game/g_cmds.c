8ea44e9a37318d6beda7493405825fac974f7a94
M. Kristall <mkpdev@gmail.com>
2009-10-03 11:50:40 +0000

    * (bug 3690) Suicides aren't cancelled when leaving/switching teams (oops)

--- src/game/g_cmds.c
+++ src/game/g_cmds.c
@@ -557,7 +557,7 @@ void G_LeaveTeam( gentity_t *self )
   G_StopFromFollowing( self );
 
   G_TeamVote( self, qfalse );
-  ent->suicideTime = 0;
+  self->suicideTime = 0;
 
   for( i = 0; i < level.num_entities; i++ )
   {
