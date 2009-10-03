ea0ff0071ed313c479693f1d2e1b9a2da167a891
Tim Angus <tim@ngus.net>
2009-10-03 21:37:08 +0000

    * Add the Q_EXPORTs I missed from merging ioq3

--- src/ui/ui_main.c
+++ src/ui/ui_main.c
@@ -145,9 +145,9 @@ void UI_SetMousePosition( int x, int y );
 void UI_Refresh( int realtime );
 qboolean UI_IsFullscreen( void );
 void UI_SetActiveMenu( uiMenuCommand_t menu );
-intptr_t vmMain( int command, int arg0, int arg1, int arg2, int arg3,
-                 int arg4, int arg5, int arg6, int arg7,
-                 int arg8, int arg9, int arg10, int arg11  )
+Q_EXPORT intptr_t vmMain( int command, int arg0, int arg1, int arg2, int arg3,
+                          int arg4, int arg5, int arg6, int arg7,
+                          int arg8, int arg9, int arg10, int arg11  )
 {
   switch( command )
   {
