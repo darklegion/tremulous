de074a1dbb77892f11c8f33f5d92be26e761e751
Asa Kravets <norfenstein@gmail.com>
2009-10-03 12:11:10 +0000

    * Dretch damage/repeat changed 24/500 -> 36/700 * Dretch value reduced 175*1.2 -> 150*1.2

--- src/game/tremulous.h
+++ src/game/tremulous.h
@@ -47,10 +47,10 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #define ABUILDER_BLOB_SPEED_MOD     0.5f
 #define ABUILDER_BLOB_TIME          5000
 
-#define LEVEL0_BITE_DMG             ADM(24)
+#define LEVEL0_BITE_DMG             ADM(36)
 #define LEVEL0_BITE_RANGE           64.0f
 #define LEVEL0_BITE_WIDTH           6.0f
-#define LEVEL0_BITE_REPEAT          500
+#define LEVEL0_BITE_REPEAT          700
 #define LEVEL0_BITE_K_SCALE         1.0f
 
 #define LEVEL1_CLAW_DMG             ADM(32)
@@ -158,7 +158,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #define ABUILDER_UPG_COST           0
 
 #define LEVEL0_SPEED                1.3f
-#define LEVEL0_VALUE                AVM(175)
+#define LEVEL0_VALUE                AVM(150)
 #define LEVEL0_HEALTH               AHM(20)
 #define LEVEL0_REGEN                1
 #define LEVEL0_COST                 0
