6e90e4e7861f5cb354487d1fe0f1fd06c385308e
Ben Millwood <thebenmachine@gmail.com>
2009-10-03 12:31:39 +0000

    * (bug 3760) Tremulous reports as ioQ3 when downloading with curl   (David Severwright)

--- src/client/cl_curl.c
+++ src/client/cl_curl.c
@@ -265,8 +265,8 @@ void CL_cURL_BeginDownload( const char *localName, const char *remoteURL )
 		qcurl_easy_setopt(clc.downloadCURL, CURLOPT_VERBOSE, 1);
 	qcurl_easy_setopt(clc.downloadCURL, CURLOPT_URL, clc.downloadURL);
 	qcurl_easy_setopt(clc.downloadCURL, CURLOPT_TRANSFERTEXT, 0);
-	qcurl_easy_setopt(clc.downloadCURL, CURLOPT_REFERER, va("ioQ3://%s",
-		NET_AdrToString(clc.serverAddress)));
+	qcurl_easy_setopt(clc.downloadCURL, CURLOPT_REFERER,
+		va("Tremulous://%s", NET_AdrToString(clc.serverAddress)));
 	qcurl_easy_setopt(clc.downloadCURL, CURLOPT_USERAGENT, va("%s %s",
 		Q3_VERSION, qcurl_version()));
 	qcurl_easy_setopt(clc.downloadCURL, CURLOPT_WRITEFUNCTION,
