902ab6c0138eb3b93b3f3cbae1c6af7d0935c038
Christopher Schwarz <lakitu7@gmail.com>
2009-10-10 07:12:38 +0000

    * Fix restart causing a crash due to the new configstrings code (kevlarman, Rezyn)

--- src/server/sv_init.c
+++ src/server/sv_init.c
@@ -38,7 +38,7 @@ static void SV_SendConfigstring(client_t *client, int index)
 	int len;
 
 	if( sv.configstrings[index].restricted && Com_ClientListContains(
-		&sv.configstrings[index].clientList, client->gentity->s.number ) ) {
+		&sv.configstrings[index].clientList, client - svs.clients ) ) {
 		// Send a blank config string for this client if it's listed
 		SV_SendServerCommand( client, "cs %i \"\"\n", index );
 		return;
