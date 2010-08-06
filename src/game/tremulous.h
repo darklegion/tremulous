3161697c566765dbc4b3200d159b98c4e233d213
Asa Kravets <norfenstein@gmail.com>
2010-08-06 00:49:38 +0000

    Increase DC heal rate 3 -> 4

--- src/game/tremulous.h
+++ src/game/tremulous.h
@@ -579,7 +579,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #define DC_SPLASHDAMAGE             50
 #define DC_SPLASHRADIUS             100
 #define DC_ATTACK_PERIOD            10000 // how often to spam "under attack"
-#define DC_HEALRATE                 3
+#define DC_HEALRATE                 4
 #define DC_RANGE                    1000
 #define DC_VALUE                    HBVM(DC_BP)
 
