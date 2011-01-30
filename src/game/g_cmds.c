b143b76b53eaf1beab387564c8209d7666244aa9
Christopher Schwarz <lakitu7@gmail.com>
2011-01-30 20:06:41 +0000

    * Fix not giving score/stage credits to aliens when humans decon dead buildables before the explosion (thanks Teapot)

--- src/game/g_cmds.c
+++ src/game/g_cmds.c
@@ -1785,6 +1785,7 @@ void Cmd_Destroy_f( gentity_t *ent )
     if( traceEnt->health <= 0 )
     {
       G_QueueBuildPoints( traceEnt );
+      G_RewardAttackers( traceEnt );
       G_FreeEntity( traceEnt );
       return;
     }
