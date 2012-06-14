c846c4bd7c090f4e55c847eb693efd5704875a89
Zachary Slater <zachary@ioquake.org>
2012-06-14 18:28:58 +0000

    CVE-2012-3345

--- src/qcommon/qcommon.h
+++ src/qcommon/qcommon.h
@@ -1089,7 +1089,6 @@ char    *Sys_DefaultAppPath(void);
 
 void  Sys_SetDefaultHomePath(const char *path);
 char	*Sys_DefaultHomePath(void);
-const char	*Sys_TempPath(void);
 const char *Sys_Dirname( char *path );
 const char *Sys_Basename( char *path );
 char *Sys_ConsoleInput(void);
