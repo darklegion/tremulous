id-Software/Quake-III-Arena

- Add HOMEPATH default name define to q_shared.c, patch by uZu (#4973) - Reverted change from (#4972) and implemented the other suggestion to be consistent with cl_cgame.c and cl_ui.c

code/qcommon/q_shared.h
@@ -34,6 +34,9 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
   #define GAMENAME_FOR_MASTER		"iofoo3"		// must NOT contain whitespaces
   #define HEARTBEAT_FOR_MASTER		GAMENAME_FOR_MASTER
   #define FLATLINE_FOR_MASTER		GAMENAME_FOR_MASTER "dead"
+  #define HOMEPATH_NAME_UNIX		".foo"
+  #define HOMEPATH_NAME_WIN		"FooBar"
+  #define HOMEPATH_NAME_MACOSX		HOMEPATH_NAME_WIN
 #else
   #define PRODUCT_NAME			"ioq3"
   #define BASEGAME			"baseq3"
@@ -42,6 +45,9 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
   #define GAMENAME_FOR_MASTER		"Quake3Arena"
   #define HEARTBEAT_FOR_MASTER		"QuakeArena-1"
   #define FLATLINE_FOR_MASTER		HEARTBEAT_FOR_MASTER
+  #define HOMEPATH_NAME_UNIX		".q3a"
+  #define HOMEPATH_NAME_WIN		"Quake3"
+  #define HOMEPATH_NAME_MACOSX		HOMEPATH_NAME_WIN
 #endif
 
 #define BASETA				"missionpack"

darklegion/tremulous

src/qcommon/q_shared.h
@@ -40,6 +40,9 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #define GAMENAME_FOR_MASTER       "Tremulous"
 #define HEARTBEAT_FOR_MASTER      GAMENAME_FOR_MASTER
 #define FLATLINE_FOR_MASTER       GAMENAME_FOR_MASTER "dead"
+#define HOMEPATH_NAME_UNIX        ".tremulous"
+#define HOMEPATH_NAME_WIN         "Tremulous"
+#define HOMEPATH_NAME_MACOSX      HOMEPATH_NAME_WIN
 
 #define MAX_TEAMNAME 32
 #define MAX_MASTER_SERVERS      5 // number of supported master servers