id-Software/Quake-III-Arena

Use GENTITYNUM_BITS for jumppad_ent bits.

code/qcommon/msg.c
@@ -1190,7 +1190,7 @@ netField_t	playerStateFields[] =
 { PSF(grapplePoint[0]), 0 },
 { PSF(grapplePoint[1]), 0 },
 { PSF(grapplePoint[2]), 0 },
-{ PSF(jumppad_ent), 10 },
+{ PSF(jumppad_ent), GENTITYNUM_BITS },
 { PSF(loopSound), 16 }
 };
 

darklegion/tremulous

src/qcommon/msg.c
@@ -1184,7 +1184,7 @@ netField_t	playerStateFields[] =
 { PSF(grapplePoint[0]), 0 },
 { PSF(grapplePoint[1]), 0 },
 { PSF(grapplePoint[2]), 0 },
-{ PSF(otherEntityNum), 10 },
+{ PSF(otherEntityNum), GENTITYNUM_BITS },
 { PSF(loopSound), 16 }
 };
 