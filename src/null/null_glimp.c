1fba10104e76e937eeac60bc207a74012ab936dc
Tim Angus <tim@ngus.net>
2013-02-15 23:46:37 +0000

    renderer -> renderergl1, rend2 -> renderergl2

--- src/null/null_glimp.c
+++ src/null/null_glimp.c
@@ -20,7 +20,7 @@ along with Tremulous; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 ===========================================================================
 */
-#include "../renderer/tr_local.h"
+#include "../renderergl1/tr_local.h"
 
 
 qboolean ( * qwglSwapIntervalEXT)( int interval );
