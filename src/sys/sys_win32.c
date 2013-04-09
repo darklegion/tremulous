41db0dd2e8c3ceabab3211f2708a4cadfb38ff36
Tim Angus <tim@ngus.net>
2013-04-09 21:41:51 +0100

    Fix shfolder.dll not getting freed

--- src/sys/sys_win32.c
+++ src/sys/sys_win32.c
@@ -91,15 +91,15 @@ char *Sys_DefaultHomePath( void )
 	TCHAR szPath[MAX_PATH];
 	FARPROC qSHGetFolderPath;
 	HMODULE shfolder = LoadLibrary("shfolder.dll");
-	
+
+	if(shfolder == NULL)
+	{
+		Com_Printf("Unable to load SHFolder.dll\n");
+		return NULL;
+	}
+
 	if(!*homePath && com_homepath)
 	{
-		if(shfolder == NULL)
-		{
-			Com_Printf("Unable to load SHFolder.dll\n");
-			return NULL;
-		}
-
 		qSHGetFolderPath = GetProcAddress(shfolder, "SHGetFolderPathA");
 		if(qSHGetFolderPath == NULL)
 		{
@@ -115,11 +115,16 @@ char *Sys_DefaultHomePath( void )
 			FreeLibrary(shfolder);
 			return NULL;
 		}
-		Q_strncpyz( homePath, szPath, sizeof( homePath ) );
-		Q_strcat( homePath, sizeof( homePath ), "\\" HOMEPATH_NAME_WIN );
-		FreeLibrary(shfolder);
+		
+		Com_sprintf(homePath, sizeof(homePath), "%s%c", szPath, PATH_SEP);
+
+		if(com_homepath->string[0])
+			Q_strcat(homePath, sizeof(homePath), com_homepath->string);
+		else
+			Q_strcat(homePath, sizeof(homePath), HOMEPATH_NAME_WIN);
 	}
 
+	FreeLibrary(shfolder);
 	return homePath;
 }
 
