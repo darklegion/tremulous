43a6fb781b481f6a07f11b9b722a3de59f830997
SmileTheory <SmileTheory@gmail.com>
2013-10-14 01:55:54 -0700

    OpenGL2: Clean up texmod calculations, and improve vertex animation handling.

--- src/renderergl2/tr_vbo.c
+++ src/renderergl2/tr_vbo.c
@@ -605,6 +605,7 @@ void R_BindVBO(VBO_t * vbo)
 		glState.vertexAttribsInterpolation = 0;
 		glState.vertexAttribsOldFrame = 0;
 		glState.vertexAttribsNewFrame = 0;
+		glState.vertexAnimation = qfalse;
 
 		qglBindBufferARB(GL_ARRAY_BUFFER_ARB, vbo->vertexesVBO);
 
@@ -853,6 +854,9 @@ void RB_UpdateVBOs(unsigned int attribBits)
 	{
 		R_BindVBO(tess.vbo);
 
+		// orphan old buffer so we don't stall on it
+		qglBufferDataARB(GL_ARRAY_BUFFER_ARB, tess.vbo->vertexesSize, NULL, GL_DYNAMIC_DRAW_ARB);
+
 		if(attribBits & ATTR_BITS)
 		{
 			if(attribBits & ATTR_POSITION)
@@ -919,6 +923,9 @@ void RB_UpdateVBOs(unsigned int attribBits)
 	if(tess.numIndexes > 0 && tess.numIndexes <= SHADER_MAX_INDEXES)
 	{
 		R_BindIBO(tess.ibo);
+
+		// orphan old buffer so we don't stall on it
+		qglBufferDataARB(GL_ELEMENT_ARRAY_BUFFER_ARB, tess.ibo->indexesSize, NULL, GL_DYNAMIC_DRAW_ARB);
 
 		qglBufferSubDataARB(GL_ELEMENT_ARRAY_BUFFER_ARB, 0, tess.numIndexes * sizeof(tess.indexes[0]), tess.indexes);
 	}
