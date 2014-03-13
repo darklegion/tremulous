7fd2d99020512adce7f16465e0814cbcdb2bb8b3
Tequila <tequila@smokin-guns.org>
2014-03-13 02:20:54 +0100

    Bunch of comment fixes

--- src/qcommon/q_shared.h
+++ src/qcommon/q_shared.h
@@ -569,7 +569,7 @@ typedef struct {
 
 #define Byte4Copy(a,b)			((b)[0]=(a)[0],(b)[1]=(a)[1],(b)[2]=(a)[2],(b)[3]=(a)[3])
 
-// just in case you do't want to use the macros
+// just in case you don't want to use the macros
 vec_t _DotProduct( const vec3_t v1, const vec3_t v2 );
 void _VectorSubtract( const vec3_t veca, const vec3_t vecb, vec3_t out );
 void _VectorAdd( const vec3_t veca, const vec3_t vecb, vec3_t out );
