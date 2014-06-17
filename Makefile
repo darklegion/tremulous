id-Software/Quake-III-Arena

bug 5986 - Configure jenkins to spit out mac .app https://bugzilla.icculus.org/show_bug.cgi?id=5986 Created make-macosx-app.sh to handle manually creating an app bundle from other scripts. Updated make-macosx.sh to create bundle with make-macosx-app.sh (TODO: make-macosx-ub.sh support). Updated Makefile to create bundle with make-macosx-app.sh and zip up the resulting ioquake3.app if ARCHIVE is defined.

Makefile
@@ -1205,6 +1205,9 @@ endif
 
 NAKED_TARGETS=$(shell echo $(TARGETS) | sed -e "s!$(B)/!!g")
 
+MACOSX_TARGET_STRING=$(shell if [ "$(B)" == "$(BR)" ]; then echo "release"; elif [ "$(B)" == "$(BD)" ]; then echo "debug"; fi)
+MACOSX_MAKE_APP=@if [ -x "./make-macosx-app.sh" ]; then "./make-macosx-app.sh" $(MACOSX_TARGET_STRING) $(ARCH); fi
+
 print_list=@for i in $(1); \
      do \
              echo "    $$i"; \
@@ -1259,9 +1262,18 @@ ifneq ($(TARGETS),)
 endif
 
 $(B).zip: $(TARGETS)
-ifdef ARCHIVE
+ifeq ($(PLATFORM),darwin)
+  ifdef ARCHIVE
+	$(call MACOSX_MAKE_APP)
+	@rm -f $@
+	@(if [ -d "$(B)/ioquake3.app" ]; then cd $(B) && zip --symlinks -r9 ../../$@ `find "ioquake3.app" -print | sed -e "s!$(B)/!!g"`; fi)
+  endif
+endif
+ifneq ($(PLATFORM),darwin)
+  ifdef ARCHIVE
 	@rm -f $@
 	@(cd $(B) && zip -r9 ../../$@ $(NAKED_TARGETS))
+  endif
 endif
 
 makedirs:

darklegion/tremulous

Makefile
@@ -1154,6 +1154,9 @@ endif
 
 NAKED_TARGETS=$(shell echo $(TARGETS) | sed -e "s!$(B)/!!g")
 
+MACOSX_TARGET_STRING=$(shell if [ "$(B)" == "$(BR)" ]; then echo "release"; elif [ "$(B)" == "$(BD)" ]; then echo "debug"; fi)
+MACOSX_MAKE_APP=@if [ -x "./make-macosx-app.sh" ]; then "./make-macosx-app.sh" $(MACOSX_TARGET_STRING) $(ARCH); fi
+
 print_list=@for i in $(1); \
      do \
              echo "    $$i"; \
@@ -1208,9 +1211,18 @@ ifneq ($(TARGETS),)
 endif
 
 $(B).zip: $(TARGETS)
-ifdef ARCHIVE
-	rm -f $@
-	(cd $(B) && zip -r9 ../../$@ $(NAKED_TARGETS))
+ifeq ($(PLATFORM),darwin)
+  ifdef ARCHIVE
+	$(call MACOSX_MAKE_APP)
+	@rm -f $@
+	@(if [ -d "$(B)/tremulous.app" ]; then cd $(B) && zip --symlinks -r9 ../../$@ `find "tremulous.app" -print | sed -e "s!$(B)/!!g"`; fi)
+  endif
+endif
+ifneq ($(PLATFORM),darwin)
+  ifdef ARCHIVE
+	@rm -f $@
+	@(cd $(B) && zip -r9 ../../$@ $(NAKED_TARGETS))
+  endif
 endif
 
 makedirs: