id-Software/Quake-III-Arena

Makefile builds fall back to traditional archiving when Mac OS X application bundle generation fails

Makefile
@@ -1205,9 +1205,6 @@ endif
 
 NAKED_TARGETS=$(shell echo $(TARGETS) | sed -e "s!$(B)/!!g")
 
-#MACOSX_TARGET_STRING=$(shell if [ "$(B)" == "$(BR)" ]; then echo "release"; elif [ "$(B)" == "$(BD)" ]; then echo "debug"; fi)
-MACOSX_MAKE_APP=@if [ -x "./make-macosx-app.sh" ]; then "./make-macosx-app.sh" release $(ARCH); fi
-
 print_list=@for i in $(1); \
      do \
              echo "    $$i"; \
@@ -1264,9 +1261,7 @@ endif
 $(B).zip: $(TARGETS)
 ifeq ($(PLATFORM),darwin)
   ifdef ARCHIVE
-	$(call MACOSX_MAKE_APP)
-	@rm -f $@
-	@(if [ -d "$(B)/ioquake3.app" ]; then cd $(B) && zip --symlinks -r9 ../../$@ `find "ioquake3.app" -print | sed -e "s!$(B)/!!g"`; fi)
+	@("./make-macosx-app.sh" release $(ARCH); if [ "$$?" -eq 0 ] && [ -d "$(B)/ioquake3.app" ]; then rm -f $@; cd $(B) && zip --symlinks -r9 ../../$@ `find "ioquake3.app" -print | sed -e "s!$(B)/!!g"`; else rm -f $@; cd $(B) && zip -r9 ../../$@ $(NAKED_TARGETS); fi)
   endif
 endif
 ifneq ($(PLATFORM),darwin)

darklegion/tremulous

Makefile
@@ -1154,9 +1154,6 @@ endif
 
 NAKED_TARGETS=$(shell echo $(TARGETS) | sed -e "s!$(B)/!!g")
 
-#MACOSX_TARGET_STRING=$(shell if [ "$(B)" == "$(BR)" ]; then echo "release"; elif [ "$(B)" == "$(BD)" ]; then echo "debug"; fi)
-MACOSX_MAKE_APP=@if [ -x "./make-macosx-app.sh" ]; then "./make-macosx-app.sh" release $(ARCH); fi
-
 print_list=@for i in $(1); \
      do \
              echo "    $$i"; \
@@ -1213,9 +1210,7 @@ endif
 $(B).zip: $(TARGETS)
 ifeq ($(PLATFORM),darwin)
   ifdef ARCHIVE
-	$(call MACOSX_MAKE_APP)
-	@rm -f $@
-	@(if [ -d "$(B)/tremulous.app" ]; then cd $(B) && zip --symlinks -r9 ../../$@ `find "tremulous.app" -print | sed -e "s!$(B)/!!g"`; fi)
+	@("./make-macosx-app.sh" release $(ARCH); if [ "$$?" -eq 0 ] && [ -d "$(B)/tremulous.app" ]; then rm -f $@; cd $(B) && zip --symlinks -r9 ../../$@ `find "tremulous.app" -print | sed -e "s!$(B)/!!g"`; else rm -f $@; cd $(B) && zip -r9 ../../$@ $(NAKED_TARGETS); fi)
   endif
 endif
 ifneq ($(PLATFORM),darwin)