b03d41c0e8aaaf631ca93e14e3f8a94f28886286
/dev/humancontroller <devhc@example.com>
2014-07-13 16:53:10 +0200

    change setviewpos to work as it sounds like
    setviewpos now precisely puts the player's view origin to the specified coordinates, and does not spit the player forward; it now optionally takes yaw and pitch arguments
    
    for this, setviewpos takes ps->viewheight into account when teleporting the player; TeleportPlayer() now takes an additional float argument specifying the spitting speed of the player; currently, TeleportPlayer() is called with a non-0 argument only from the setviewpos function

--- src/game/g_mover.c
+++ src/game/g_mover.c
@@ -1169,7 +1169,7 @@ static void Touch_DoorTriggerSpectator( gentity_t *ent, gentity_t *other, trace_
   }
 
   vectoangles( dir, angles );
-  TeleportPlayer( other, origin, angles );
+  TeleportPlayer( other, origin, angles, 400.0f );
 }
 
 
