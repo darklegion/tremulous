502d5b81898df2420cdc3601c1cb6d711844eaa5
Zack Middleton <zturtleman@gmail.com>
2014-09-29 00:35:03 -0500

    OpenGL2: Don't segfault if r_ext_multitexture is disabled

--- src/renderergl2/tr_shader.c
+++ src/renderergl2/tr_shader.c
@@ -2371,7 +2371,7 @@ static void CollapseStagesToLightall(shaderStage_t *diffuse,
 }
 
 
-static qboolean CollapseStagesToGLSL(void)
+static int CollapseStagesToGLSL(void)
 {
 	int i, j, numStages;
 	qboolean skip = qfalse;
@@ -3108,7 +3108,9 @@ static shader_t *FinishShader( void ) {
 	//
 	// look for multitexture potential
 	//
-	stage = CollapseStagesToGLSL();
+	if ( qglActiveTextureARB ) {
+		stage = CollapseStagesToGLSL();
+	}
 
 	if ( shader.lightmapIndex >= 0 && !hasLightmapStage ) {
 		if (vertexLightmap) {
