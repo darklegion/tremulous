6d2d70c7fd0d6f56269f611146e65a715aa7898a
SmileTheory <SmileTheory@gmail.com>
2014-11-10 21:59:37 -0800

    OpenGL2: remove lightmap support from generic glsl shader. This path was barely used and doing this compiles fewer shaders.

--- src/renderergl2/tr_shade.c
+++ src/renderergl2/tr_shade.c
@@ -1352,16 +1352,6 @@ static void RB_IterateStagesGeneric( shaderCommands_t *input )
 		else if ( pStage->bundle[1].image[0] != 0 )
 		{
 			R_BindAnimatedImageToTMU( &pStage->bundle[0], 0 );
-
-			//
-			// lightmap/secondary pass
-			//
-			if ( r_lightmap->integer ) {
-				GLSL_SetUniformInt(sp, UNIFORM_TEXTURE1ENV, GL_REPLACE);
-			} else {
-				GLSL_SetUniformInt(sp, UNIFORM_TEXTURE1ENV, tess.shader->multitextureEnv);
-			}
-
 			R_BindAnimatedImageToTMU( &pStage->bundle[1], 1 );
 		}
 		else 
@@ -1370,8 +1360,6 @@ static void RB_IterateStagesGeneric( shaderCommands_t *input )
 			// set state
 			//
 			R_BindAnimatedImageToTMU( &pStage->bundle[0], 0 );
-
-			GLSL_SetUniformInt(sp, UNIFORM_TEXTURE1ENV, 0);
 		}
 
 		//
