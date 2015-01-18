d9580d71cd7e5edcd57eed506454c67db35db211
Ensiform <ensiform@gmail.com>
2015-01-17 22:52:00 -0600

    Move Event processor call down below the mouse checks.
    Only send mouse events if both values are non-zero.
    
    Hopefully this helps with the event overflow spam that can sometimes
    happen on loads or laggy situations.

--- src/sdl/sdl_input.c
+++ src/sdl/sdl_input.c
@@ -851,7 +851,11 @@ static void IN_ProcessEvents( void )
 
 			case SDL_MOUSEMOTION:
 				if( mouseActive )
+				{
+					if( !e.motion.xrel && !e.motion.yrel )
+						break;
 					Com_QueueEvent( 0, SE_MOUSE, e.motion.xrel, e.motion.yrel, 0, NULL );
+				}
 				break;
 
 			case SDL_MOUSEBUTTONDOWN:
@@ -933,7 +937,6 @@ void IN_Frame( void )
 	int x, y;
 
 	IN_JoyMove( );
-	IN_ProcessEvents( );
 
 	// If not DISCONNECTED (main menu) or ACTIVE (in game), we're loading
 	loading = ( clc.state != CA_DISCONNECTED && clc.state != CA_ACTIVE );
@@ -967,6 +970,8 @@ void IN_Frame( void )
 		SDL_GetMouseState( &x, &y );
 		IN_SetUIMousePosition( x, y );
 	}
+
+	IN_ProcessEvents( );
 
 	// In case we had to delay actual restart of video system
 	if( ( vidRestartTime != 0 ) && ( vidRestartTime < Sys_Milliseconds( ) ) )
