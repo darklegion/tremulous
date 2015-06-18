6acd341a52a6c4ac10324125adcd11618f7fa320
/dev/humancontroller <devhc@example.com>
2015-06-18 11:16:40 -0500

    let fs_basegame default to gpp

--- src/qcommon/files.c
+++ src/qcommon/files.c
@@ -3196,7 +3196,7 @@ static void FS_Startup( const char *gameName )
 
 	fs_debug = Cvar_Get( "fs_debug", "0", 0 );
 	fs_basepath = Cvar_Get ("fs_basepath", Sys_DefaultInstallPath(), CVAR_INIT|CVAR_PROTECTED );
-	fs_basegame = Cvar_Get ("fs_basegame", "", CVAR_INIT );
+	fs_basegame = Cvar_Get ("fs_basegame", "gpp", CVAR_INIT );
 	homePath = Sys_DefaultHomePath();
 	if (!homePath || !homePath[0]) {
 		homePath = fs_basepath->string;
