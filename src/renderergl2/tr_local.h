6f6ca6d529e1739565dbc64891fa7f1b478b8460
SmileTheory <SmileTheory@gmail.com>
2015-12-09 02:00:40 -0800

    OpenGL2: Parse radius entries from misc_cubemap entities.

--- src/renderergl2/tr_local.h
+++ src/renderergl2/tr_local.h
@@ -61,6 +61,12 @@ typedef unsigned int glIndex_t;
 #define USE_VERT_TANGENT_SPACE
 #define USE_OVERBRIGHT
 
+typedef struct cubemap_s {
+	vec3_t origin;
+	float parallaxRadius;
+	image_t *image;
+} cubemap_t;
+
 typedef struct dlight_s {
 	vec3_t	origin;
 	vec3_t	color;				// range from 0.0 to 1.0, should be color normalized
@@ -1561,8 +1567,7 @@ typedef struct {
 	int		                fatLightmapStep;
 
 	int                     numCubemaps;
-	vec3_t                  *cubemapOrigins;
-	image_t                 **cubemaps;
+	cubemap_t               *cubemaps;
 
 	trRefEntity_t			*currentEntity;
 	trRefEntity_t			worldEntity;		// point currentEntity at this when rendering world
