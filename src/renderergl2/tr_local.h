40b91090cef5cd2479433f9c7a02f52ddde58f3e
SmileTheory <SmileTheory@gmail.com>
2016-07-26 00:41:31 -0700

    OpenGL2: Use loader for all extension funcs.

--- src/renderergl2/tr_local.h
+++ src/renderergl2/tr_local.h
@@ -1403,7 +1403,6 @@ typedef struct {
 	qboolean textureNonPowerOfTwo;
 	qboolean textureFloat;
 	qboolean halfFloatPixel;
-	qboolean packedDepthStencil;
 	textureCompressionRef_t textureCompression;
 	qboolean swizzleNormalmap;
 	
