a8eb13bdd41dfc37fbd2ecd387a7cd5b2c94c001
Victor Roemer <victor@badsec.org>
2016-08-14 03:13:49 -0400

    add g++-multilib

--- misc/travis-ci-before.sh
+++ misc/travis-ci-before.sh
@@ -2,7 +2,7 @@
 platform=$(uname | sed -e 's/_.*//' | tr '[:upper:]' '[:lower:]' | sed -e 's/\//_/g')
 if [[ "$platform" == "linux" ]]; then
     sudo apt-get update -qq &> /dev/null
-    sudo apt-get install -q -y libgl1-mesa-dev libsdl2-dev libfreetype6-dev mingw-w64 g++-mingw-w64 &> /dev/null
+    sudo apt-get install -q -y libgl1-mesa-dev libsdl2-dev libfreetype6-dev mingw-w64 g++-mingw-w64 g++-multilib &> /dev/null
 else
     brew update &> /dev/null
     brew install freetype &> /dev/null
