85f6a2b0132417140ec70fee2d4b28ddc9926b7a
Victor Roemer <victor@badsec.org>
2016-08-14 03:14:47 -0400

    fix

--- misc/travis-ci-build.sh
+++ misc/travis-ci-build.sh
@@ -2,7 +2,7 @@
 PLATFORM=$(uname | sed -e 's/_.*//' | tr '[:upper:]' '[:lower:]' | sed -e 's/\//_/g')
 failed=0
 
-(make -k lean release release-${PLATFORM}-${ARCH}.zip) || failed=1
+(make clean release release-${PLATFORM}-${ARCH}.zip) || failed=1
 
 if [[ $failed -eq 1 ]]; then
 	echo "Build failure."
