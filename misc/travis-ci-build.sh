75f4c614039da54b79054ae3fa0ca952297eb7ac
Victor Roemer <victor@badsec.org>
2016-09-10 11:18:03 -0400

    Enable verbose

--- misc/travis-ci-build.sh
+++ misc/travis-ci-build.sh
@@ -3,7 +3,7 @@ P=$(uname | sed -e 's/_.*//' | tr '[:upper:]' '[:lower:]' | sed -e 's/\//_/g')
 PLATFORM=${PLATFORM:-$P}
 failed=0
 
-(make clean release) || failed=1
+(make V=1 clean release) || failed=1
 
 if [[ $failed -eq 1 ]]; then
 	echo "Build failure."
