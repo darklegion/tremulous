2b182dddb2f4940a8770e172ece118eb9e560b94
Victor Roemer <victor@badsec.org>
2017-04-03 10:18:20 -0400

    Update the package with the game paks

--- misc/download-paks.sh
+++ misc/download-paks.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+URL="https://github.com/wtfbbqhax/tremulous-data/raw/master/"
+
 packages="
-https://github.com/wtfbbqhax/tremulous-data/raw/master/data-1.1.0.pk3
-https://github.com/wtfbbqhax/tremulous-data/raw/master/data-gpp1.pk3
-https://github.com/wtfbbqhax/tremulous-data/raw/master/map-arachnid2-1.1.0.pk3
-https://github.com/wtfbbqhax/tremulous-data/raw/master/map-atcs-1.1.0.pk3
-https://github.com/wtfbbqhax/tremulous-data/raw/master/map-karith-1.1.0.pk3
-https://github.com/wtfbbqhax/tremulous-data/raw/master/map-nexus6-1.1.0.pk3
-https://github.com/wtfbbqhax/tremulous-data/raw/master/map-niveus-1.1.0.pk3
-https://github.com/wtfbbqhax/tremulous-data/raw/master/map-travis-1.1.0.pk3
-https://github.com/wtfbbqhax/tremulous-data/raw/master/map-tremor-1.1.0.pk3
-https://github.com/wtfbbqhax/tremulous-data/raw/master/map-uncreation-1.1.0.pk3
+data-1.1.0.pk3
+data-gpp1.pk3
+map-arachnid2-1.1.0.pk3
+map-atcs-1.1.0.pk3
+map-karith-1.1.0.pk3
+map-nexus6-1.1.0.pk3
+map-niveus-1.1.0.pk3
+map-travis-1.1.0.pk3
+map-tremor-1.1.0.pk3
+map-uncreation-1.1.0.pk3
 "
 
 for dir in ./build/*; do
@@ -18,11 +20,32 @@ for dir in ./build/*; do
         continue;
     fi
 
-    pushd $dir/gpp
+    if [[ $dir == "./build/release-darwin-x86_64" ]]; then
+        echo "Got here"
+        pushd $dir/Tremulous.app/Contents/MacOS/gpp/ 
+    else
+        pushd $dir/gpp
+    fi
 
     for i in $packages; do
-        wget $i
+        if [[ -e $package ]]; then
+            rm -f $package
+        fi
+        wget $URL/$i
     done
 
     popd
+
+    # Repackage
+
+    if [[ $dir == "./build/release-darwin-x86_64" ]]; then
+        pushd $dir
+        zip -r ../$(basename $dir).zip Tremulous.app
+    else
+        pushd build/
+        zip -r $(basename $dir).zip $(basename $dir)/gpp/*.pk3
+    fi
+
+    popd
 done
+
