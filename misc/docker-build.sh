04a2843d77db5dea6208c7fe4106c51de6eb1219
Victor Roemer <victor@badsec.org>
2017-04-04 01:22:17 -0400

    Fail build if any docker target fails
    Fix win32

--- misc/docker-build.sh
+++ misc/docker-build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-USE_RESTCLIENT=1 USE_INTERNAL_LUA=1 PLATFORM=mingw32 make -j 2
-USE_RESTCLIENT=1 USE_INTERNAL_LUA=1 PLATFORM=linux make -j 2
+failed=0
+
+USE_RESTCLIENT=1 USE_INTERNAL_LUA=1 PLATFORM=mingw32 make -j 2 || failed=1
+USE_RESTCLIENT=1 USE_INTERNAL_LUA=1 PLATFORM=linux make -j 2 || failed=1
+
+if [[ $failed -eq 1 ]]; then
+    echo "Build failure."
+    exit $failed
+fi
+
 ./misc/download-paks.sh
 chmod -R ugo+rw build
