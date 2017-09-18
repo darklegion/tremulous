f84cf93d534c95ce6f5e35838da85c31b67195af
Zack Middleton <zack@cloemail.com>
2017-09-17 20:52:10 -0400

    client: Remove CVAR_PROTECTED from cl_renderer
    Setting cl_renderer isn't a security concern and valid behavior for a
    menu to provide. Sys_LoadDLL ensures only libries are loaded and engine
    prevents QVMs from writing them.

--- src/client/cl_main.cpp
+++ src/client/cl_main.cpp
@@ -4672,7 +4672,7 @@ static void CL_InitRef(void)
     Com_Printf("----- Initializing Renderer ----\n");
 
 #ifdef USE_RENDERER_DLOPEN
-    cl_renderer = Cvar_Get("cl_renderer", "opengl2", CVAR_ARCHIVE | CVAR_LATCH | CVAR_PROTECTED);
+    cl_renderer = Cvar_Get("cl_renderer", "opengl2", CVAR_ARCHIVE | CVAR_LATCH);
 
     Com_sprintf(dllName, sizeof(dllName), "renderer_%s" DLL_EXT, cl_renderer->string);
 
